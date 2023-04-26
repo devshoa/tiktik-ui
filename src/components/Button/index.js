f43f040bb09222ece611c8c3815c2be590629380
HoaCoder <VoThaiHoa19102003@gmail.com>
2023-04-26 18:43:02 +0700

    Bài 59

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import myAlert from "~/myAlert"
+
+function Button() {
+    return(
+        <div>
+            <button onClick={myAlert}>Click me!</button>
+        </div>
+    )
+}
+
+export default Button
